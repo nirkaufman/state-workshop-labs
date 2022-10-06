a77ce97f8dd31e8799b49f5f0a8ad429e894b391
nir.k <nir.k@next-insurance.com>
2022-10-06 15:29:59 +0300

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {SyncStartComponent} from "./lab_2/sync-start.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [SyncStartComponent],
   template: `
     <div class="container">
         <h1>Sate Management Workshop</h1>
+        <app-sync-start></app-sync-start>
     </div>
   `,
 })
---
+++ src/app/lab_2/lab_2_instructions.md
@@ -0,0 +1,6 @@
+# LAB II : UI Sync
+
+- Implement the list logic on a separate class.
+- When item is add: show the item count on the footer.
+- On the header, show a message: 'Last item added at: <date here>'
+- Ui components should not know about each other
